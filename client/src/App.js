194517567bec176abc8b46facde1e55a06cfae15
romasinha <sinha.roma99@gmail.com>
2022-01-24 06:54:13 +0530

    Routes set

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+
+
+
+import Join from "./components/Join";
+import Chat from "./components/Chat";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path = "/" element = {<Join/>}/>
+          <Route path = "/chat" element = {<Chat/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
